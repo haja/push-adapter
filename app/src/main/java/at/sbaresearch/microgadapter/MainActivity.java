b8c9091b8276939ebabce1af6ff33af3c2c75872
haja <h.jagenteufel@gmail.com>
2018-11-21 13:17:28 +0100

    MainActivity: remove dead code

--- app/src/main/java/at/sbaresearch/microgadapter/MainActivity.java
+++ app/src/main/java/at/sbaresearch/microgadapter/MainActivity.java
@@ -1,22 +1,16 @@
 package at.sbaresearch.microgadapter;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-  private static final int REVB_PERM_REQ = 1;
-  private static final String RECV_PERM = "at.sbaresearch.android.c2dm.intent.RECEIVE";
+  private static final String TAG = MainActivity.class.getSimpleName();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +25,8 @@ public class MainActivity extends AppCompatActivity {
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        /*
-        if (ContextCompat.checkSelfPermission(activity, RECV_PERM)
-            != PackageManager.PERMISSION_GRANTED) {
-          ActivityCompat.requestPermissions(activity, new String[]{RECV_PERM}, REVB_PERM_REQ);
-        } else {
-          Toast.makeText(activity, "perm granted", Toast.LENGTH_SHORT).show();
-        }
-        */
       }
     });
-  }
-
-  @Override
-  public void onRequestPermissionsResult(int requestCode, String permissions[],
-      int[] grantResults) {
-    switch (requestCode) {
-      case REVB_PERM_REQ: {
-        if (grantResults.length > 0
-            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-          Toast.makeText(this, "perm granted", Toast.LENGTH_SHORT).show();
-        } else {
-          Toast.makeText(this, "perm NOT granted", Toast.LENGTH_SHORT).show();
-        }
-        break;
-      }
-    }
   }
 
   @Override
