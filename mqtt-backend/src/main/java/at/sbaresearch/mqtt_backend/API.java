2d97625545aed1c0d9f96e353a9a910e7922910c
haja <h.jagenteufel@gmail.com>
2018-11-22 16:22:50 +0100

    mqtt BE: message structure WIP

---
+++ mqtt-backend/src/main/java/at/sbaresearch/mqtt_backend/API.java
@@ -0,0 +1,6 @@
+package at.sbaresearch.mqtt_backend;
+
+public class API {
+  public static String payload = "payload";
+  public static String id = "id";
+}
--- mqtt-backend/src/main/java/at/sbaresearch/mqtt_backend/ReceiveCallback.java
+++ mqtt-backend/src/main/java/at/sbaresearch/mqtt_backend/ReceiveCallback.java
@@ -2,10 +2,17 @@ package at.sbaresearch.mqtt_backend;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.JsonReader;
 import android.util.Log;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.Reader;
 
 class ReceiveCallback implements MqttCallback {
   private static final String TAG = "ReceiveCallback";
@@ -26,20 +33,26 @@ class ReceiveCallback implements MqttCallback {
   @Override
   public void messageArrived(String topic, MqttMessage message) throws Exception {
     Log.i(TAG, "MQTT msg recv: " + message.toString());
-    Payload pay = parsePayload(message.getPayload());
+    Payload pay = parsePayload(message);
     sendIntent(pay);
   }
 
-  private Payload parsePayload(byte[] payload) {
-    // TODO parse json payload
-    return null;
+  private Payload parsePayload(MqttMessage msg) throws JSONException {
+    // TODO do actual json parsing instead of toString
+    JSONObject obj = (JSONObject) new JSONTokener(msg.toString()).nextValue();
+    String id = obj.getString("id");
+    String payload = obj.getString("payload");
+    return new Payload(id, payload);
   }
 
   private void sendIntent(Payload payload) {
+    // TODO register intent, well-dfine intent constant
     Intent intent = new Intent(INTENT_MQTT_RECEIVE);
     // TODO raise intent with payload
     // TODO put data from mqtt backend here (appID? Identity hash? msg-content?)
     // intent.putExtra(GCM_API.EXTRA_FROM, "testPushApp");
+    intent.putExtra(API.id, payload.id);
+    intent.putExtra(API.payload, payload.payload);
 
     context.sendBroadcast(intent);
   }
@@ -49,6 +62,15 @@ class ReceiveCallback implements MqttCallback {
   }
 
   private class Payload {
+
     // TODO what fields are needed?
+    private final String id;
+    // TODO this should be a map? or binary?
+    private final String payload;
+
+    public Payload(String id, String payload) {
+      this.id = id;
+      this.payload = payload;
+    }
   }
 }
