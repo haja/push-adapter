3e6a2a0ecb5830551c3961a30e6ea51ef092be4d
haja <h.jagenteufel@gmail.com>
2018-11-29 12:43:00 +0100

    sample-backend: actually parse JSON rest requests

--- sample-backend/src/main/java/at/sbaresearch/mqtt4android/sample/backend/AppResource.java
+++ sample-backend/src/main/java/at/sbaresearch/mqtt4android/sample/backend/AppResource.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class AppResource {
 
   @RequestMapping(value = "/register", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
   //public void register(HttpServletRequest req) {
-  public void register(AppRegistrationRequest request) {
+  public void register(@RequestBody AppRegistrationRequest request) {
     log.info("register called with registrationId {}", request.registrationId);
     // TODO link with app instance / userId.. mocked for now
     this.currentRegId = Option.of(request.registrationId);
@@ -55,7 +56,7 @@ public class AppResource {
   }
 
   @Value
-  private class AppRegistrationRequest {
+  public static class AppRegistrationRequest {
     String registrationId;
   }
 }
