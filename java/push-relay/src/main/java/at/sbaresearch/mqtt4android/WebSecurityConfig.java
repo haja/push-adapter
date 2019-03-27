3d4c63ae87aaaa1a6666bdb6a51ae95e8fbee72b
haja <h.jagenteufel@gmail.com>
2019-03-27 14:31:58 +0100

    relay: disable http auth from spring security

---
+++ java/push-relay/src/main/java/at/sbaresearch/mqtt4android/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package at.sbaresearch.mqtt4android;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    // TODO configure http security
+    http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+  }
+}
