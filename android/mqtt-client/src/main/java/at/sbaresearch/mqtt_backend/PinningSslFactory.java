3cca770ba56bc36d0dadbcd3a386b945a77b05e0
haja <h.jagenteufel@gmail.com>
2019-03-30 10:26:41 +0100

    android: working private key loading for ssl pinning

--- android/mqtt-client/src/main/java/at/sbaresearch/mqtt_backend/PinningSslFactory.java
+++ android/mqtt-client/src/main/java/at/sbaresearch/mqtt_backend/PinningSslFactory.java
@@ -7,15 +7,15 @@ import javax.net.SocketFactory;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.spec.*;
 
 public class PinningSslFactory {
 
@@ -26,27 +26,21 @@ public class PinningSslFactory {
 
   PinningSslFactory(Context applicationContext) throws Exception {
     Certificate ca = getCertificate(applicationContext.getResources().openRawResource(R.raw.server));
-    KeyStore caKeyStore = createKeystore(ca, "ca");
+    KeyStore caKeyStore = from(ca, "ca");
 
     // Create a TrustManager that trusts the CAs in our KeyStore
     String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
     trustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm);
     trustManagerFactory.init(caKeyStore);
 
-    // TODO load client cert from memory
-    Certificate clientCert = getCertificate(applicationContext.getResources().openRawResource(R.raw.client));
-    KeyStore clientStore = createKeystore(clientCert, "client");
+    // TODO load client key from memory
+    Key clientKey = getKey(applicationContext.getResources().openRawResource(R.raw.client_key));
+    Certificate[] clientCert = new Certificate[] {
+        getCertificate(applicationContext.getResources().openRawResource(R.raw.client_cert))
+    };
+    KeyStore clientStore = from(clientKey, "client", clientCert);
     keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
     keyManagerFactory.init(clientStore, null);
-  }
-
-  private KeyStore createKeystore(Certificate certificate, String alias)
-      throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
-    String keyStoreType = KeyStore.getDefaultType();
-    KeyStore keyStore = KeyStore.getInstance(keyStoreType);
-    keyStore.load(null, null);
-    keyStore.setCertificateEntry(alias, certificate);
-    return keyStore;
   }
 
   private Certificate getCertificate(final InputStream inputStream)
@@ -57,6 +51,46 @@ public class PinningSslFactory {
       Log.d(TAG, "getCertificate: ca=" + ((X509Certificate) ca).getSubjectDN());
       return ca;
     }
+  }
+
+  private KeyStore from(Certificate certificate, String alias)
+      throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
+    KeyStore keyStore = createKeyStore();
+    keyStore.setCertificateEntry(alias, certificate);
+    return keyStore;
+  }
+
+  private Key getKey(InputStream inputStream)
+      throws NoSuchAlgorithmException, IOException, InvalidKeySpecException {
+    KeyFactory kf = KeyFactory.getInstance("EC");
+    try (InputStream in = inputStream) {
+      byte[] bytes = toByteArray(in);
+      KeySpec spec = new PKCS8EncodedKeySpec(bytes);
+      return kf.generatePrivate(spec);
+    }
+  }
+
+  private byte[] toByteArray(InputStream in) throws IOException {
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    while (in.available() > 0) {
+      bos.write(in.read());
+    }
+    return bos.toByteArray();
+  }
+
+  private KeyStore from(Key key, String alias, Certificate[] chain)
+      throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
+    KeyStore keyStore = createKeyStore();
+    keyStore.setKeyEntry(alias, key, null, chain);
+    return keyStore;
+  }
+
+  private KeyStore createKeyStore()
+      throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
+    String keyStoreType = KeyStore.getDefaultType();
+    KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+    keyStore.load(null, null);
+    return keyStore;
   }
 
   public SocketFactory getSocketFactory() throws Exception {
