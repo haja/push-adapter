ce25177bc6a6ee421cc76e56c041700d3b11a4c5
haja <h.jagenteufel@gmail.com>
2019-04-06 22:06:10 +0200

    relay: move cert setup helpers

--- java/push-relay/setup-helpers/init-server-and-ca.sh
+++ java/push-relay/setup-helpers/init-server-and-ca.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-
+# TODO this is unfinished
 keytool -genkey -keyalg EC -alias server -validity 360 -keystore cert-stores/keystore.p12 -storetype pkcs12
 keytool -genkey -keyalg EC -alias ca -validity 360 -keystore cert-stores/castore.p12 -storetype pkcs12 -ext BC=ca:true
