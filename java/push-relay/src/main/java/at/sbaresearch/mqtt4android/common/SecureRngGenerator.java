28dd0b99cd72546918fb3e26ff7331847dcddf77
haja <h.jagenteufel@gmail.com>
2019-08-13 13:36:43 +0200

    Revert "push-relay: log SecureRngGenerator static creation exception"
    This reverts commit 58460489409beb7174b1ad690de0525df3fddc71.

--- java/push-relay/src/main/java/at/sbaresearch/mqtt4android/common/SecureRngGenerator.java
+++ java/push-relay/src/main/java/at/sbaresearch/mqtt4android/common/SecureRngGenerator.java
@@ -1,6 +1,5 @@
 package at.sbaresearch.mqtt4android.common;
 
-import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +8,6 @@ import java.security.SecureRandom;
 import java.util.Base64;
 
 @Component
-@Slf4j
 public class SecureRngGenerator {
   private final static SecureRandom RNG;
 
@@ -17,7 +15,6 @@ public class SecureRngGenerator {
     try {
       RNG = SecureRandom.getInstanceStrong();
     } catch (NoSuchAlgorithmException e) {
-      log.error("cannot get SecureRandom", e);
       throw new RuntimeException(e);
     }
   }
