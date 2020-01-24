376d73f991d5882228468e87412ca5f763cefa05
haja <h.jagenteufel@gmail.com>
2020-01-24 13:41:21 +0100

    push-relay + app-backend: add LICENSE (GPLv3) + license headers

--- java/app-backend/src/main/java/at/sbaresearch/mqtt4android/sample/backend/SampleAppBackendApplication.java
+++ java/app-backend/src/main/java/at/sbaresearch/mqtt4android/sample/backend/SampleAppBackendApplication.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020 Harald Jagenteufel.
+ *
+ * This file is part of push-relay.
+ *
+ *     push-relay is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     push-relay is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with push-relay.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package at.sbaresearch.mqtt4android.sample.backend;
 
 import org.springframework.boot.SpringApplication;
