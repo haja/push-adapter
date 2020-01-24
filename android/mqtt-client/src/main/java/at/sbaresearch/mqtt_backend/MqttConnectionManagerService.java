ddedf36690d34e2d2c3ebcdfbfbb7e128e7c61e4
haja <h.jagenteufel@gmail.com>
2020-01-24 13:15:12 +0100

    mqtt-client: add LICENSE and license headers

--- android/mqtt-client/src/main/java/at/sbaresearch/mqtt_backend/MqttConnectionManagerService.java
+++ android/mqtt-client/src/main/java/at/sbaresearch/mqtt_backend/MqttConnectionManagerService.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2020 Harald Jagenteufel.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html.
+ */
+
 package at.sbaresearch.mqtt_backend;
 
 import android.Manifest.permission;
